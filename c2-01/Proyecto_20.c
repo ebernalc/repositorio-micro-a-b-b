90f9034a30cfb8768dd0adef43bdeb1e09e6a03e
ebernalc <ebernalchiquillo@gmail.com>
2025-04-21 17:47:31 -0500

    Update Proyecto_20.c

--- c2-01/Proyecto_20.c
+++ c2-01/Proyecto_20.c
@@ -5,41 +5,35 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-#define touch TOUCH_PAD_NUM0 // GPIO-4 o pin 26
+#define touch TOUCH_PAD_NUM0 
 #define potenciometro ADC_CHANNEL_6
-#define ADC_WIDTH ADC_WIDTH_BIT_12  // Resolución de 12 bits (0-4095)
-#define ADC_ATTEN ADC_ATTEN_DB_11   // Rango hasta 3.3V
-
+#define ADC_WIDTH ADC_WIDTH_BIT_12  
+#define ADC_ATTEN ADC_ATTEN_DB_11   
 float ADC_VOLT_MAX = 3.3;
 uint16_t touch_value;
 
 void app_main(){
-    // Configurar ADC
     adc1_config_width(ADC_WIDTH);
     adc1_config_channel_atten(potenciometro, ADC_ATTEN);
 
-    // Esperar comando '1' + Enter
     char ch;
     printf("Esperando comando '1' + Enter para iniciar...\n");
 
     while (1) {
         ch = getchar();
         if (ch == '1') {
-            // Esperar a que se presione Enter después del '1'
             ch = getchar();
             if (ch == '\r' || ch == '\n') {
                 printf("Comando correcto recibido. Iniciando lectura táctil y ADC...\n");
                 break;
             }
         }
-        vTaskDelay(pdMS_TO_TICKS(100)); // Pequeño retardo para evitar saturación
+        vTaskDelay(pdMS_TO_TICKS(100)); 
     }
 
-    // Iniciar pin táctil
     touch_pad_init();
     touch_pad_config(touch, 0);
 
-    // Bucle principal
     while (1)
     {
         touch_pad_read(touch, &touch_value);
@@ -50,6 +44,6 @@ void app_main(){
             printf("ADC: %d - %.2f%%\n", raw, porcentaje);
         }
 
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Espera 1 segundo
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
